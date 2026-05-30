657634b3a0a563f5edf1f2902f9ee1ff16544f99
azizweb2030-create <azizweb2030@gmail.com>
2026-05-30 06:58:10 +0300

    fix: add serverActions config + trigger redeploy

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '*.supabase.co' }]
+  },
+  experimental: {
+    serverActions: { allowedOrigins: ['*'] }
   }
 }
 module.exports = nextConfig
